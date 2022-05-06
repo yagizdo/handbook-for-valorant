c0eb6e7877c03dc30cd63a2640dbf1e2c3acd64a
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-06 20:25:09 +0300

    added app_colors.dart | update app.dart theme

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const black = Color(0xFF323232);
+const white = Color(0xFFFFFFFF);
+const grey = Color(0xFF787878);
+
+class AppTheme {
+  final valoTheme = ThemeData(
+      scaffoldBackgroundColor: black,
+      appBarTheme: AppBarTheme(backgroundColor: black));
+}
