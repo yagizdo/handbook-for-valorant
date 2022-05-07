ccecc0bccf1de637f2dd92c2e147932a7591f335
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-07 17:11:07 +0300

    deleted tab bar

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -22,44 +22,9 @@ class _MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
       text: 'Others',
     ),
   ];
-  TabController? _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(vsync: this, length: tabs.length);
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        toolbarHeight: 0,
-        bottom: TabBar(
-          indicatorWeight: 0.001,
-          indicatorSize: TabBarIndicatorSize.label,
-          controller: _tabController,
-          tabs: tabs
-        ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-          Container(
-            color: Colors.blue,
-          ),
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.yellow,
-          ),
-          Container(
-            color: Colors.black,
-          ),
-        ],
-      ),
-    );
+    return Scaffold();
   }
 }
