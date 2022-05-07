687758160b83a385ed4213820534871ba8c9e85d
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-07 22:26:26 +0300

    others screen added

---
+++ lib/screens/others_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OthersScreen extends StatelessWidget {
+  const OthersScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Text('Others'),
+      )),
+    );
+  }
+}
