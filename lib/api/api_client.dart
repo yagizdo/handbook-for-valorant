560f163baa61a2cd18306ace81080e2f9e7f8ddc
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-07 23:55:56 +0300

    updated api_client

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -1,5 +1,21 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:valorant_tips/models/agents.dart';
 
 class ApiClient{
+Dio _dio = Dio();
 
+final String _baseUrl = 'https://valorant-api.com/';
+
+Future<Agents> getAgents() async {
+  Response agentData = await _dio.get(_baseUrl + 'v1/agents/');
+
+
+  Agents agents = Agents.fromJson(agentData.data);
+
+  print('User info : ${agents.data.displayName}');
+  print('User info : ${agents.data.role}');
+
+  return agents;
+}
 }
