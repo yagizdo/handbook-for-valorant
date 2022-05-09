618b9bcfd949aa1e088987f710ac1180da630acd
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-09 23:45:18 +0300

    added desc

--- lib/widgets/agents_screen/Agent/agent_info.dart
+++ lib/widgets/agents_screen/Agent/agent_info.dart
@@ -10,42 +10,57 @@ class AgentInfo extends StatelessWidget {
   Agent agent;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-          alignment: Alignment.bottomLeft,
-          children: [
-        Column(
-          children: [CachedNetworkImage(imageUrl: agent.fullPortraitV2!)],
-        ),
-        Positioned(
-          left: 15.w,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              agent.displayName!,
-              style: TextStyle(
-                  fontFamily: 'Valorant', color: white, fontSize: 50.sp,fontWeight: FontWeight.w600),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                CachedNetworkImage(imageUrl: agent.role!.displayIcon!,width: 12.5.w,),
-                SizedBox(width: 5.w,),
-                Padding(
-                  padding: EdgeInsets.only(top: 1.h),
-                  child: Text(
-                    agent.role!.displayName!,
-                    style: TextStyle(
-                        fontFamily: 'Valorant', color: white, fontSize: 11.sp),
-                  ),
-                ),
-              ],
-            )
-          ],
-        ))
-      ]),
+    return Column(
+      children: [
+        agentImage(),
+        Padding(
+          padding: EdgeInsets.only(top: 15.h,left: 15.w,right: 15.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(agent.description!,style: const TextStyle(color: white,),textAlign: TextAlign.start,),
+            ],
+          ),
+        )
+      ],
     );
+  }
+
+  Stack agentImage() {
+    return Stack(
+      alignment: Alignment.bottomLeft,
+      children: [
+    Column(
+      children: [CachedNetworkImage(imageUrl: agent.fullPortraitV2!)],
+    ),
+    Positioned(
+      left: 15.w,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          agent.displayName!,
+          style: TextStyle(
+              fontFamily: 'Valorant', color: white, fontSize: 50.sp,fontWeight: FontWeight.w600),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            CachedNetworkImage(imageUrl: agent.role!.displayIcon!,width: 12.5.w,),
+            SizedBox(width: 5.w,),
+            Padding(
+              padding: EdgeInsets.only(top: 1.h),
+              child: Text(
+                agent.role!.displayName!,
+                style: TextStyle(
+                    fontFamily: 'Valorant', color: white, fontSize: 11.sp),
+              ),
+            ),
+          ],
+        )
+      ],
+    ),)
+  ]);
   }
 }
