cea42f24ef478f238b059df0f752ee1332ae1ed0
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-10 00:03:29 +0300

    added agent info

--- lib/widgets/agents_screen/Agent/agent_info.dart
+++ lib/widgets/agents_screen/Agent/agent_info.dart
@@ -13,54 +13,72 @@ class AgentInfo extends StatelessWidget {
     return Column(
       children: [
         agentImage(),
-        Padding(
-          padding: EdgeInsets.only(top: 15.h,left: 15.w,right: 15.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(agent.description!,style: const TextStyle(color: white,),textAlign: TextAlign.start,),
-            ],
-          ),
-        )
+        agentBio(),
       ],
     );
   }
 
+  Padding agentBio() {
+    return Padding(
+      padding: EdgeInsets.only(top: 15.h, left: 15.w, right: 15.w),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            agent.description!,
+            style: TextStyle(
+              color: white,
+              fontSize: 14.5.sp
+            ),
+            textAlign: TextAlign.start,
+          ),
+        ],
+      ),
+    );
+  }
+
   Stack agentImage() {
-    return Stack(
-      alignment: Alignment.bottomLeft,
-      children: [
-    Column(
-      children: [CachedNetworkImage(imageUrl: agent.fullPortraitV2!)],
-    ),
-    Positioned(
-      left: 15.w,
+    return Stack(alignment: Alignment.bottomLeft, children: [
+      Column(
+        children: [CachedNetworkImage(imageUrl: agent.fullPortraitV2!)],
+      ),
+      Positioned(
+        left: 15.w,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          agent.displayName!,
-          style: TextStyle(
-              fontFamily: 'Valorant', color: white, fontSize: 50.sp,fontWeight: FontWeight.w600),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            CachedNetworkImage(imageUrl: agent.role!.displayIcon!,width: 12.5.w,),
-            SizedBox(width: 5.w,),
-            Padding(
-              padding: EdgeInsets.only(top: 1.h),
-              child: Text(
-                agent.role!.displayName!,
-                style: TextStyle(
-                    fontFamily: 'Valorant', color: white, fontSize: 11.sp),
-              ),
+            Text(
+              agent.displayName!,
+              style: TextStyle(
+                  fontFamily: 'Valorant',
+                  color: white,
+                  fontSize: 50.sp,
+                  fontWeight: FontWeight.w600),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                CachedNetworkImage(
+                  imageUrl: agent.role!.displayIcon!,
+                  width: 12.5.w,
+                ),
+                SizedBox(
+                  width: 5.w,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 1.h),
+                  child: Text(
+                    agent.role!.displayName!,
+                    style: TextStyle(
+                        fontFamily: 'Valorant', color: white, fontSize: 11.sp),
+                  ),
+                ),
+              ],
+            )
           ],
-        )
-      ],
-    ),)
-  ]);
+        ),
+      )
+    ]);
   }
 }
