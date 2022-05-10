f59fa8e8073e5d47d09936404fc28de53f547cd6
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-11 00:25:49 +0300

    added mapsclient, maps model and get data from maps api

--- lib/screens/maps_screen.dart
+++ lib/screens/maps_screen.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:valorant_tips/models/map.dart';
+import 'package:valorant_tips/network/maps_client.dart';
 
-class MapsScreen extends StatelessWidget {
+class MapsScreen extends StatefulWidget {
   const MapsScreen({Key? key}) : super(key: key);
 
+  @override
+  State<MapsScreen> createState() => _MapsScreenState();
+}
+
+class _MapsScreenState extends State<MapsScreen> {
+  MapsClient mapsClient = MapsClient();
+
+  late Future<Iterable<Maps>> maps;
+  @override
+  void initState() {
+    maps = mapsClient.getlAllMaps();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
