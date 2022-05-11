a0184cfeea075524e98713a4d45036e4a1fae5fc
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-11 22:18:41 +0300

    added ontap in mapscard

--- lib/widgets/maps_screen/maps_card.dart
+++ lib/widgets/maps_screen/maps_card.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:valorant_tips/constants/app_colors.dart';
+import 'package:valorant_tips/widgets/maps_screen/maps_detail.dart';
 
 import '../../models/map.dart';
 
@@ -10,24 +11,29 @@ class MapsCard extends StatelessWidget {
   Maps map;
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Container(
-          width: 1.sw,
-          height: 100,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            image: DecorationImage(
-              image: CachedNetworkImageProvider(map.listViewIcon!),
-            )
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) => MapsDetail()));
+      },
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            width: 1.sw,
+            height: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              image: DecorationImage(
+                image: CachedNetworkImageProvider(map.listViewIcon!),
+              )
+            ),
           ),
-        ),
 
-        Positioned(child: Text(map.displayName!,style: TextStyle(color: white,fontFamily: 'Valorant',fontSize: 20.sp),),
-        )
+          Positioned(child: Text(map.displayName!,style: TextStyle(color: white,fontFamily: 'Valorant',fontSize: 20.sp),),
+          )
 
-      ],
+        ],
+      ),
     );
   }
 }
