b784127f96bc6921b47a39485ee01b28d094c3df
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-13 17:35:12 +0300

    updated rank client and rank screen

--- lib/models/rank.dart
+++ lib/models/rank.dart
@@ -1,83 +1,84 @@
-import 'package:meta/meta.dart';
-import 'dart:convert';
-
-
 class Rank {
-  Rank({
-    required this.uuid,
-    required this.assetObjectName,
-    required this.tiers,
-    required this.assetPath,
-  });
+  String? uuid;
+  String? assetObjectName;
+  List<Tiers>? tiers;
+  String? assetPath;
 
-  String uuid;
-  String assetObjectName;
-  List<Tier> tiers;
-  String assetPath;
+  Rank({this.uuid, this.assetObjectName, this.tiers, this.assetPath});
 
-  factory Rank.fromJson(Map<String, dynamic> json) => Rank(
-    uuid: json["uuid"],
-    assetObjectName: json["assetObjectName"],
-    tiers: List<Tier>.from(json["tiers"].map((x) => Tier.fromJson(x))),
-    assetPath: json["assetPath"],
-  );
+  Rank.fromJson(Map<String, dynamic> json) {
+    uuid = json['uuid'];
+    assetObjectName = json['assetObjectName'];
+    if (json['tiers'] != null) {
+      tiers = <Tiers>[];
+      json['tiers'].forEach((v) {
+        tiers!.add(Tiers.fromJson(v));
+      });
+    }
+    assetPath = json['assetPath'];
+  }
 
-  Map<String, dynamic> toJson() => {
-    "uuid": uuid,
-    "assetObjectName": assetObjectName,
-    "tiers": List<dynamic>.from(tiers.map((x) => x.toJson())),
-    "assetPath": assetPath,
-  };
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['uuid'] = this.uuid;
+    data['assetObjectName'] = this.assetObjectName;
+    if (this.tiers != null) {
+      data['tiers'] = this.tiers!.map((v) => v.toJson()).toList();
+    }
+    data['assetPath'] = this.assetPath;
+    return data;
+  }
 }
 
-class Tier {
-  Tier({
-    required this.tier,
-    required this.tierName,
-    required this.division,
-    required this.divisionName,
-    required this.color,
-    required this.backgroundColor,
-    required this.smallIcon,
-    required this.largeIcon,
-    required this.rankTriangleDownIcon,
-    required this.rankTriangleUpIcon,
-  });
+class Tiers {
+  int? tier;
+  String? tierName;
+  String? division;
+  String? divisionName;
+  String? color;
+  String? backgroundColor;
+  String? smallIcon;
+  String? largeIcon;
+  String? rankTriangleDownIcon;
+  String? rankTriangleUpIcon;
 
-  int tier;
-  String tierName;
-  String division;
-  String divisionName;
-  String color;
-  String backgroundColor;
-  String smallIcon;
-  String largeIcon;
-  String rankTriangleDownIcon;
-  String rankTriangleUpIcon;
+  Tiers(
+      {this.tier,
+        this.tierName,
+        this.division,
+        this.divisionName,
+        this.color,
+        this.backgroundColor,
+        this.smallIcon,
+        this.largeIcon,
+        this.rankTriangleDownIcon,
+        this.rankTriangleUpIcon});
 
-  factory Tier.fromJson(Map<String, dynamic> json) => Tier(
-    tier: json["tier"],
-    tierName: json["tierName"],
-    division: json["division"],
-    divisionName: json["divisionName"],
-    color: json["color"],
-    backgroundColor: json["backgroundColor"],
-    smallIcon: json["smallIcon"] == null ? null : json["smallIcon"],
-    largeIcon: json["largeIcon"] == null ? null : json["largeIcon"],
-    rankTriangleDownIcon: json["rankTriangleDownIcon"] == null ? null : json["rankTriangleDownIcon"],
-    rankTriangleUpIcon: json["rankTriangleUpIcon"] == null ? null : json["rankTriangleUpIcon"],
-  );
+  Tiers.fromJson(Map<String, dynamic> json) {
+    tier = json['tier'];
+    tierName = json['tierName'];
+    division = json['division'];
+    divisionName = json['divisionName'];
+    color = json['color'];
+    backgroundColor = json['backgroundColor'];
+    smallIcon = json['smallIcon'];
+    largeIcon = json['largeIcon'];
+    rankTriangleDownIcon = json['rankTriangleDownIcon'];
+    rankTriangleUpIcon = json['rankTriangleUpIcon'];
+  }
 
-  Map<String, dynamic> toJson() => {
-    "tier": tier,
-    "tierName": tierName,
-    "division": division,
-    "divisionName": divisionName,
-    "color": color,
-    "backgroundColor": backgroundColor,
-    "smallIcon": smallIcon == null ? null : smallIcon,
-    "largeIcon": largeIcon == null ? null : largeIcon,
-    "rankTriangleDownIcon": rankTriangleDownIcon == null ? null : rankTriangleDownIcon,
-    "rankTriangleUpIcon": rankTriangleUpIcon == null ? null : rankTriangleUpIcon,
-  };
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['tier'] = this.tier;
+    data['tierName'] = this.tierName;
+    data['division'] = this.division;
+    data['divisionName'] = this.divisionName;
+    data['color'] = this.color;
+    data['backgroundColor'] = this.backgroundColor;
+    data['smallIcon'] = this.smallIcon;
+    data['largeIcon'] = this.largeIcon;
+    data['rankTriangleDownIcon'] = this.rankTriangleDownIcon;
+    data['rankTriangleUpIcon'] = this.rankTriangleUpIcon;
+    return data;
+  }
 }
--- lib/screens/ranks_screen.dart
+++ lib/screens/ranks_screen.dart
@@ -1,15 +1,62 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:valorant_tips/network/rank_client.dart';
 
-class RanksScreen extends StatelessWidget {
+import '../models/rank.dart';
+
+class RanksScreen extends StatefulWidget {
   const RanksScreen({Key? key}) : super(key: key);
 
+  @override
+  State<RanksScreen> createState() => _RanksScreenState();
+}
+
+class _RanksScreenState extends State<RanksScreen> {
+RankClient _rankClient = RankClient();
+
+late Future<Iterable<Rank>> _ranks;
+  @override
+  void initState() {
+    print('calisti init');
+    _ranks = _rankClient.getAllRanks();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Center(
-        child: Text('Ranks'),
-      )),
+          child: FutureBuilder<Iterable<Rank>>(
+            future: _ranks,
+            builder: (
+                BuildContext context,
+                AsyncSnapshot<Iterable<Rank>> snapshot,
+                ) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: SizedBox(height:100.h,width:50.w,child: const CircularProgressIndicator()));
+              } else if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  return const Text('Error');
+                } else if (snapshot.hasData) {
+                  return GridView.builder(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,),
+                      // Burda - 3 deme sebebim ; Asagida da 3 index onden baslattigimiz icin burda da 3 index onden baslatmazsak
+                      // range hatasi aliriz bu yuzden burda ki lenght den de 3 cikardim
+                      itemCount: snapshot.data!.toList()[3].tiers!.length - 3,
+                      itemBuilder: (context, index) {
+                        var data = snapshot.data!.toList();
+                        // Burda index + 3 deme sebebim en basta unranked + 2 tane gereksiz rank geliyordu.
+                        // Bu gereksizleri onlemek icin + 3 diyerek ana ranklari aldim
+                        return CachedNetworkImage(imageUrl: data[3].tiers![index + 3].smallIcon ?? 'https://via.placeholder.com/150');
+                      });
+                } else {
+                  return const Text('Empty data');
+                }
+              } else {
+                return Text('State: ${snapshot.connectionState}');
+              }
+            }),
+          )
     );
   }
 }
