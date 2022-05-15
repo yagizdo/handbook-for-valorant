6d431efa46d159efab3cfde12cf956a5e7dda4e1
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-16 00:07:54 +0300

    added human body for damage datas

--- lib/constants/app_assets.dart
+++ lib/constants/app_assets.dart
@@ -2,4 +2,6 @@ class AppAssets {
   final agents_icon = 'lib/constants/icons/agent_icon.png';
   final weapons_icon = 'lib/constants/icons/weapons_icon.png';
   final ranks_icon = 'lib/constants/icons/rank_icon.png';
+
+  final human_full = 'lib/constants/img/human_full.png';
 }
