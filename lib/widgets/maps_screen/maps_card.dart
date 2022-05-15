a907ac1b586dd38ceef7d29848a45b39b6994e98
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-16 01:35:49 +0300

    deleted unsused import

--- lib/widgets/maps_screen/maps_card.dart
+++ lib/widgets/maps_screen/maps_card.dart
@@ -2,7 +2,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:valorant_tips/constants/app_colors.dart';
-import 'package:valorant_tips/widgets/maps_screen/maps_detail.dart';
 
 import '../../models/map.dart';
 
