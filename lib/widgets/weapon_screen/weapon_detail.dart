1861b73c6885f073b221c84c40b78548e04b0b2d
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-15 21:01:59 +0300

    added weapon detail

---
+++ lib/widgets/weapon_screen/weapon_detail.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../../models/weapon.dart';
+ 
+class WeaponDetail extends StatelessWidget {
+  WeaponDetail({Key? key,required this.weapon}) : super(key: key);
+  Weapon weapon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+
+      ),
+    );
+  }
+}
