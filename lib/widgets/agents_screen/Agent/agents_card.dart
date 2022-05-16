1f5b6f5e626e89f872673bb8ad1da29d35d2fe41
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-16 18:24:36 +0300

    updated agent color system

--- lib/widgets/agents_screen/Agent/agents_card.dart
+++ lib/widgets/agents_screen/Agent/agents_card.dart
@@ -14,138 +14,6 @@ class AgentsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var agentColors = [
-      // Fade
-      [
-        Colors.black87,
-        Colors.blueGrey,
-      ],
-      // Breach
-      [
-        const Color(0xFFBC542D),
-        const Color(0xFF58394D),
-        const Color(0xFFA06C45),
-        const Color(0xFF50785D)
-      ],
-      // Raze
-      [
-        const Color(0xFFe36230),
-        const Color(0xFFd89e4f),
-        const Color(0xFF2c4446),
-      ],
-      // Chamber
-      [
-        const Color(0xFF30aae1),
-        const Color(0xFFc83d7b),
-        const Color(0xFF947961),
-        const Color(0xFFffce6f)
-      ],
-      // Kay/o
-      [
-        const Color(0xFF2bc8ff),
-        const Color(0xFF4343b2),
-        const Color(0xFF905ef6),
-        const Color(0xFFff89ff)
-      ],
-      // Skye
-      [
-        const Color(0xFF8ea36f),
-        const Color(0xFF364241),
-        const Color(0xFFae8e4d),
-        const Color(0xFFaf694d)
-      ],
-      // Cypher
-      [
-        const Color(0xFFd18a5b),
-        const Color(0xFF6097df),
-        const Color(0xFF48486c),
-        const Color(0xFF4b76cc)
-      ],
-      // Sova
-      [
-        const Color(0xFFebccae),
-        const Color(0xFF2947c3),
-        const Color(0xFF997b58),
-        const Color(0xFF567fb5)
-      ],
-      // Killjoy
-      [
-        const Color(0xFFe6d759),
-        const Color(0xFF58956a),
-        const Color(0xFFdd8df8),
-        const Color(0xFFf3ab3d)
-      ],
-      // Viper
-      [
-        const Color(0xFF51af58),
-        const Color(0xFF6b8690),
-        const Color(0xFFa5ae60),
-        const Color(0xFFdef2ac)
-      ],
-      // Phoneix
-      [
-        const Color(0xFFf7bf60),
-        const Color(0xFFea6d4e),
-        const Color(0xFF838ea6),
-        const Color(0xFF7c5b60)
-      ],
-      // Astra
-      [
-        const Color(0xFFef90af),
-        const Color(0xFF4e26c9),
-        const Color(0xFF7aa1e8),
-        const Color(0xFFb0ae84)
-      ],
-      // Brimstone
-      [
-        const Color(0xFFb35723),
-        const Color(0xFF8b81aa),
-        const Color(0xFF5f1c0a),
-        const Color(0xFFeb953f)
-      ],
-      // Neon
-      [
-        const Color(0xFF00008b),
-        const Color(0xFF00FFFF),
-        const Color(0xFF00ffff),
-        const Color(0xFF00008b)
-      ],
-      // Yoru
-      [
-        const Color(0xFFe18437),
-        const Color(0xFF99412a),
-        const Color(0xFF394278),
-        const Color(0xFF0024f5)
-      ],
-      // Sage
-      [
-        const Color(0xFF9254b8),
-        const Color(0xFF67abdd),
-        const Color(0xFF66357e),
-        const Color(0xFF78fad8)
-      ],
-      // Reyna
-      [
-        const Color(0xFFce53a8),
-        const Color(0xFF2f2664),
-        const Color(0xFF383b3d),
-        const Color(0xFFe46c76)
-      ],
-      // Omen
-      [
-        const Color(0xFF7c7fb4),
-        const Color(0xFF3e3c56),
-        const Color(0xFF4a5fa4),
-        const Color(0xFF59bbc5)
-      ],
-      // Jett
-      [
-        const Color(0xFF9edaf6),
-        const Color(0xFF5266aa),
-        const Color(0xFF2b577c),
-        const Color(0xFF90fcfd)
-      ],
-    ];
     return GestureDetector(
       onTap: () {
         Navigator.push(
@@ -168,7 +36,66 @@ class AgentsCard extends StatelessWidget {
                 gradient: LinearGradient(
                     begin: Alignment.topLeft,
                     end: Alignment.bottomRight,
-                    colors: agentColors[index]),
+                    colors:
+                    // Fade
+                    agent.displayName!.toLowerCase() == 'fade' ? fadeColors
+                        :
+                    // Breach
+                    agent.displayName!.toLowerCase() == 'breach' ? breachColors
+                        :
+                    // Raze
+                    agent.displayName!.toLowerCase() == 'raze' ? razeColors
+                        :
+                    // Chamber
+                    agent.displayName!.toLowerCase() == 'chamber' ? chamberColors
+                        :
+                    // Kayo
+                    agent.displayName!.toLowerCase() == 'kay/o' ? kayoColors
+                        :
+                    // Skye
+                    agent.displayName!.toLowerCase() == 'skye' ? skyeColors
+                        :
+                    // Cypher
+                    agent.displayName!.toLowerCase() == 'cypher' ? cypherColors
+                        :
+                    // Sova
+                    agent.displayName!.toLowerCase() == 'sova' ? sovaColors
+                        :
+                    // Killjoy
+                    agent.displayName!.toLowerCase() == 'killjoy' ? killjoyColors
+                        :
+                    // Viper
+                    agent.displayName!.toLowerCase() == 'viper' ? viperColors
+                        :
+                    // Phoneix
+                    agent.displayName == 'Phoenix' ? phoneixColors
+                        :
+                    // Astra
+                    agent.displayName!.toLowerCase() == 'astra' ? astraColors
+                        :
+                    // Brimstone
+                    agent.displayName!.toLowerCase() == 'brimstone' ? brimstoneColors
+                        :
+                    // Neon
+                    agent.displayName!.toLowerCase() == 'neon' ? neonColors
+                        :
+                    // Yoru
+                    agent.displayName!.toLowerCase() == 'yoru' ? yoruColors
+                        :
+                    // Sage
+                    agent.displayName!.toLowerCase() == 'sage' ? sageColors
+                        :
+                    // Reyna
+                    agent.displayName!.toLowerCase() == 'reyna' ? reynaColors
+                        :
+                    // Omen
+                    agent.displayName!.toLowerCase() == 'omen' ? omenColors
+                        :
+                    // Jett
+                    agent.displayName!.toLowerCase() == 'jett' ? jettColors
+                        :
+                    [Colors.black87,
+                      Colors.blueGrey,]),
                 borderRadius: const BorderRadius.all(
                   Radius.circular(10),
                 ),
