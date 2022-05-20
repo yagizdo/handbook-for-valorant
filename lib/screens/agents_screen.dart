b48ffc2220c89a07ccde2bad48a6ce208824a828
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-21 01:31:07 +0300

    agent screen ui fix

--- lib/screens/agents_screen.dart
+++ lib/screens/agents_screen.dart
@@ -70,40 +70,47 @@ class _AgentsScreenState extends State<AgentsScreen> {
                 scrollDirection: Axis.horizontal,
                 itemCount: agentFilters.length,
                 itemBuilder: (context, index) {
-                  return Padding(
-                    padding: EdgeInsets.all(Platform.isAndroid ? 8.w :10.w),
-                    // Gesture detector for index update
-                    child: GestureDetector(
-                      onTap: (){
-                        if(agentFilters[index].toLowerCase() == 'all') {
-                         setState(() {
-                           agents = _agentClient.getAgents();
-                           selectedFilterIndex = index;
-                         });
-                        } else {
-                          setState(() {
-                            agents = _agentClient.getAgents(agentRole: agentFilters[index]);
-                            selectedFilterIndex = index;
-                          });
-                        }
+                  return LayoutBuilder(
+                    builder: (context, constraints) {
+                      return Padding(
+                        padding: EdgeInsets.all(Platform.isAndroid ? 8.w :
+                        // iPhone 7 - 38 and iPhone 5S - SE 1. Gen - 32
+                        constraints.maxHeight.round() == 38 || constraints.maxHeight.round() == 32 ? 7.h :
+                        // Others
+                        10.w),
+                        // Gesture detector for index update
+                        child: GestureDetector(
+                          onTap: (){
+                            if(agentFilters[index].toLowerCase() == 'all') {
+                              setState(() {
+                                agents = _agentClient.getAgents();
+                                selectedFilterIndex = index;
+                              });
+                            } else {
+                              setState(() {
+                                agents = _agentClient.getAgents(agentRole: agentFilters[index]);
+                                selectedFilterIndex = index;
+                              });
+                            }
 
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            // If box selected bg color red and text color white
-                            // If box unselected bg color white and text color black
-                            color: selectedFilterIndex == index ? CupertinoColors.systemRed : CupertinoColors.white),
-                        width: 100.w,
-                        alignment: Alignment.center,
-                        child: Text(
-                          agentFilters[index],
-                          style: TextStyle(color: selectedFilterIndex == index ? white : black , fontFamily: 'Valorant',fontSize: 10.5.sp),
-                          textAlign: TextAlign.center,
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                // If box selected bg color red and text color white
+                                // If box unselected bg color white and text color black
+                                color: selectedFilterIndex == index ? CupertinoColors.systemRed : CupertinoColors.white),
+                            width: 100.w,
+                            alignment: Alignment.center,
+                            child: Text(
+                              agentFilters[index],
+                              style: TextStyle(color: selectedFilterIndex == index ? white : black , fontFamily: 'Valorant',fontSize: 10.5.sp),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  );
+                      );
+                    });
                 },
               ),
             ),
