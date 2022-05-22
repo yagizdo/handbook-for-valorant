a32ffef5bf1d4093b4a0193bcc795cd554b90012
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-22 15:28:08 +0300

    added new network system

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:valorant_tips/constants/app_colors.dart';
 import 'package:valorant_tips/screens/error_screen.dart';
 import 'package:valorant_tips/screens/main_screen.dart';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         title: 'Valo Guide',
         debugShowCheckedModeBanner: false,
         // internet check
-        home: internetInfo == ConnectivityResult.none ? ErrorScreen() : const MainScreen(),
+        home: internetInfo == InternetConnectionStatus.disconnected ? ErrorScreen() : const MainScreen(),
         theme: AppTheme().valoTheme,
       ),
     );
