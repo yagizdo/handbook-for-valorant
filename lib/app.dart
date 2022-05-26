bc21b58a251782424ddfffee07733e89c6b77db5
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-26 19:43:52 +0300

    added custom error screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:valorant_tips/constants/app_colors.dart';
+import 'package:valorant_tips/screens/custom_error_screen.dart';
 import 'package:valorant_tips/screens/error_screen.dart';
 import 'package:valorant_tips/screens/main_screen.dart';
 
@@ -16,10 +18,16 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(375,812),
       builder: (child) => MaterialApp(
+        builder: (BuildContext context, Widget? widget) {
+          ErrorWidget.builder = (FlutterErrorDetails errorDetails) {
+            return kDebugMode ? CustomErrorScreen(errorDetails: errorDetails) : ErrorScreen(errorDetails: errorDetails,isNetworkError: false,) ;
+          };
+          return widget!;
+        },
         title: Platform.isAndroid ?  'Guide for Valo' : 'Handbook for Valorant',
         debugShowCheckedModeBanner: false,
         // internet check
-        home: internetInfo == InternetConnectionStatus.disconnected ? ErrorScreen() : const MainScreen(),
+        home: internetInfo == InternetConnectionStatus.disconnected ? ErrorScreen(isNetworkError: true,) : const MainScreen(),
         theme: AppTheme().valoTheme,
       ),
     );
--- lib/screens/error_screen.dart
+++ lib/screens/error_screen.dart
@@ -4,8 +4,10 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../constants/app_colors.dart';
 
 class ErrorScreen extends StatelessWidget {
-  const ErrorScreen({Key? key}) : super(key: key);
-
+  const ErrorScreen({Key? key, required this.isNetworkError, this.errorDetails})
+      : super(key: key);
+  final bool isNetworkError;
+  final FlutterErrorDetails? errorDetails;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,10 +15,25 @@ class ErrorScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('app_icon/android_icon.png',scale: 3,),
-            Text('Something went wrong',style: TextStyle(color: white,fontSize: 20.sp),),
-            SizedBox(height: 10.h,),
-            Text('Please check your internet connection',style: TextStyle(color: white,fontSize: 15.sp),),
+            Image.asset(
+              'app_icon/android_icon.png',
+              scale: 3,
+            ),
+            Text(
+              'Something went wrong',
+              style: TextStyle(color: white, fontSize: 22.sp),
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            isNetworkError
+                ? Text('Please check your internet connection',
+                    style: TextStyle(color: white, fontSize: 15.sp))
+                : Text(
+                    'Please restart the Application',
+                    style: TextStyle(color: white, fontSize: 15.sp),
+              textAlign: TextAlign.center,
+                  ),
           ],
         ),
       ),
